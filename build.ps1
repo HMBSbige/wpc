1906653e41d6a0e808df1e88c950279f1fcbf9e5
Bruce Wayne <admin@bige0.com>
2022-04-29 12:10:43 +0800

    housekeeping: Update CI

---
+++ build.ps1
@@ -0,0 +1,26 @@
+param([string]$rid = 'win-x64')
+$ErrorActionPreference = 'Stop'
+
+Write-Host 'dotnet SDK info'
+dotnet --info
+
+$net_tfm = 'net6.0'
+$configuration = 'Release'
+$output_dir = "$PSScriptRoot\wpc\bin\$configuration"
+$proj_path = "$PSScriptRoot\wpc\wpc.csproj"
+
+function New-App
+{
+    param([string]$rid)
+    Write-Host 'Building'
+
+    $outdir = "$output_dir\$net_tfm"
+    $publishDir = "$outdir\publish"
+
+    Remove-Item $publishDir -Recurse -Force -Confirm:$false -ErrorAction Ignore
+
+    dotnet publish "$proj_path" -c $configuration -f $net_tfm -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true --self-contained true -p:PublishTrimmed=True -r $rid
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
+
+New-App $rid
